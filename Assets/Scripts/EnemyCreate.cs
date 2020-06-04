8d1a71221ce24d72de5298324aaf11ccb29cc377
Aleksey Volokontsev <a.volokontsev@yandex.ru>
2020-06-04 18:06:18 +0300

    Created

---
+++ Assets/Scripts/EnemyCreate.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyCreate : MonoBehaviour
+{
+    [SerializeField] private Transform _spawn;
+    [SerializeField] private GameObject _enemy;
+
+    [HideInInspector] private Transform[] _spawnPoints;
+
+    private void Start()
+    {
+        _spawnPoints = new Transform[_spawn.childCount];
+        for (int i = 0; i < _spawn.childCount; i++)
+        {
+            _spawnPoints[i] = _spawn.GetChild(i);
+        }
+        var createEnemy = StartCoroutine(CreateEnemy());
+    }
+
+    private IEnumerator CreateEnemy()
+    {
+        var waitSecods = new WaitForSeconds(2);
+            Debug.Log(_spawn.childCount);
+        
+        for (uint i = 0; i < uint.MaxValue; i++)
+        {            
+            GameObject enemy = Instantiate(_enemy, _spawnPoints[Random.Range(0, _spawn.childCount - 1)]);
+
+            yield return waitSecods;            
+        }
+    }
+
+
+
+
+}
---
+++ Assets/Scripts/SpiderMove.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpiderMove : MonoBehaviour
+{
+    [SerializeField] private float _speed;
+
+    void Update()
+    {
+        transform.Translate(0, _speed * Time.deltaTime, 0);
+    }
+}
