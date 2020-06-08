809ccd7fceceec341ec37a9e66469d9f4aa9ec72
Volokonce <a.volokotsev@volokonce.ru>
2020-06-08 19:36:14 +0300

    Скорректированы недочеты

--- Assets/Scripts/Spawn.cs
+++ Assets/Scripts/Spawn.cs
@@ -6,6 +6,7 @@ public class Spawn : MonoBehaviour
 {
     [SerializeField] private Transform _spawn;
     [SerializeField] private Spider _template;
+    [SerializeField] private float _spwanTimer;
 
     private Transform[] _spawnPoints;
 
@@ -16,17 +17,17 @@ public class Spawn : MonoBehaviour
         {
             _spawnPoints[i] = _spawn.GetChild(i);
         }
-        var createEnemy = StartCoroutine(CreateEnemy());
+        StartCoroutine(CreateEnemy());
     }
 
     private IEnumerator CreateEnemy()
     {
-        var waitSecods = new WaitForSeconds(2);
+        var waitSecods = new WaitForSeconds(_spwanTimer);
             Debug.Log(_spawn.childCount);
         
         for (uint i = 0; i < uint.MaxValue; i++)
         {            
-            var enemy = Instantiate(_template, _spawnPoints[Random.Range(0, _spawn.childCount - 1)]);
+            Instantiate(_template, _spawnPoints[Random.Range(0, _spawn.childCount - 1)]);
 
             yield return waitSecods;            
         }
